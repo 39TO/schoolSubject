ea5cc9391de3cc77663d182314a15608aef450a7
39TO <taiyo.murase39@gmail.com>
2023-05-30 00:12:48 +0900

    init

---
+++ Main.java
@@ -0,0 +1,33 @@
+// TODO
+// 1. BeautifulWomanクラスにメソッドを追加する
+// 2. BeautifulWomanクラスからインスタンスを生成する
+// 3. インスタンスのメソッドを呼び出す
+// 4. コードとコンソールを含めてスクリーンショットを撮る
+// 5. 提出する（お疲れ）
+
+class BeautifulWoman {
+    // フィールド
+	public String name;
+    public String face; // 顔
+    public int age; //年齢
+    public String hometown; // 出身
+    public String category; // 系統
+    public String existence = "美女"; // 存在
+    
+    // コンストラクタ
+    BeautifulWoman(String name, String face, int age, String hometown, String category) {
+    	this.name = name;
+    	this.face = face;
+    	this.age = age;
+    	this.hometown = hometown;
+    	this.category = category;
+    }
+    
+    // メソッド
+}
+
+public class Main {
+	public static void main(String[] args) {
+		// 自分だけの美女を作成しよう！！！
+	}
+}
